576cb0cfea79e46f0339c75c987543d0e81dfff4
cpkopp12 <cameronpkopp@gmail.com>
2023-05-13 13:37:30 -0600

    done

--- mathform/src/app/math-validators.ts
+++ mathform/src/app/math-validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from '@angular/forms';
 
 export class MathValidators {
   static addition(target: string, sourceOne: string, sourceTwo: string) {
-    return (form: AbstractControl<any, any>) => {
+    return (form: AbstractControl) => {
       const sum = form.value[target];
       const firstNumber = form.value[sourceOne];
       const secondNumber = form.value[sourceTwo];
